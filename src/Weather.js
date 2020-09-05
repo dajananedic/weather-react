adf950c765dd68002148145b2fbcd0288563c41d
Dajana Nedic <dajananedicuark@gmail.com>
2020-09-04 21:58:53 -0500

    added axios

---
+++ src/Weather.js
@@ -0,0 +1,15 @@
+import React from "react";
+import axios from "axios";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.name} is ${response.data.main.temp}°C`
+    );
+  }
+
+  let apiKey = "d20a58d648eb51d7e157c9054ad03816";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+  return <h2>Oh hi!</h2>;
+}
